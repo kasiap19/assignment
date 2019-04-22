887e471b3e4718a866783e0ce6df02976335fbb8
Kasia Pieniazek <kasia@mbp-kasia.mynet>
2019-04-22 20:37:16 +0100

    displaying products in the component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,43 @@
 <template>
   <div id="app">
+    <Products :products="products"/>
   </div>
 </template>
 
 <script>
+import Products from './components/Products.vue';
 
 export default {
   name: 'app',
   components: {
+    Products,
+  },
+
+  data() {
+    return {
+      products: []
+    }
+  },
+
+  created () {
+    fetch('https://5c78274f6810ec00148d0ff1.mockapi.io/api/v1/products')
+        .then(response => response.json())
+        .then((data) => {
+        this.products = data;
+
+        console.log(this.products)
+    });
   },
 };
 </script>
 
 <style lang="scss">
+@import 'assets/_settings.scss';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
---
+++ src/components/Products.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="products-component">
+        <div class="container products">
+            <div class="products__title">
+                <h1>results</h1>
+            </div>
+            <div class="products__content">
+                <div class="product" v-for="product in products" :key="product.id">
+                    <div class="product__img" :style="{ backgroundImage: 'url(' + product.img + ')' }"></div>
+                    <div class="product__likes">
+                        <button></button>
+                    </div>
+                    <div class="product__desc desc"> 
+                        <div class="desc__title">
+                            <h3>{{product.title}}</h3>
+                            <span>{{product.brand}}</span>
+                        </div>
+                        <div class="desc__sum">
+                            <span>size: {{product.size}}</span>
+                            <span class="desc__sum--price">{{product.price}} £</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Products',
+    props: ['products']
+}
+</script>
+
+<style lang="scss">
+@import '../assets/_settings.scss';
+
+.products-component {
+    background: #f1f2f6;
+    padding: 50px 0;
+}
+
+.products {
+
+    &__title {
+        h1 {
+            text-transform: uppercase;
+            font-weight: 900;
+            font-size: 26px; 
+        }
+    }
+    
+    &__content {
+        display: grid;
+        grid-row-gap: 16px;
+
+        @include md {
+            grid-template-columns: repeat(2, 1fr);
+            grid-gap: 16px;
+        }
+
+        @include lg {
+            grid-template-columns: repeat(4, 1fr);
+            grid-gap: 32px;
+        }
+    }
+}
+
+.product {
+    position: relative;
+    background: white;
+
+    &__img {
+        height: 150px;
+        background-size: cover;
+        background-position: center;
+        border-radius: 5px 5px 0 0;
+    }
+
+    &__desc {
+        padding: 20px;
+    }
+
+    &__likes {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+
+        button {
+            &:before {
+                content: '\f164';
+                font-family: "Font Awesome 5 Free";
+                font-size: 15px;
+                color: $blue;
+                font-weight: bold;
+                transition: 0;
+            }
+
+            border-radius: 5px;
+            padding: 7px;
+            background: white;
+        }
+    }
+}
+
+.desc {
+    &__title {
+        height: 50px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        h3 {
+            font-size: 16px;
+        }
+
+        span {
+            font-size: 13px;
+            font-weight: 400;
+            font-style: italic;
+        }
+    }
+
+    &__sum {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        &--price {
+            font-size: 24px;
+            font-weight: 600;
+        }
+    }
+}
+
+</style>
